306f4a03d3d5eca0bd89c8054852d75c2468e125
Manojnv2001 <146865534+Manojnv2001@users.noreply.github.com>
2024-01-04 15:14:47 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,16 @@
+﻿public partial class Program
+{
+    public static void Main(string[] args){
+    for (int i=1; i<=6; ++i)  {
+        if(i==2){
+            continue;
+        }
+            for (int j = 1; j <= i; ++j) {
+                Console.Write("*");
+            }
+            Console.WriteLine( );
+            }
+        }
+}
+        
+    
